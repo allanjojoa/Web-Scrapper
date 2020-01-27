b82e2d9aaf18106be5f669f9285eb42f6caf634c
Allan Jojo <allanjojoa@gmail.com>
2020-01-28 01:31:02 +0530

    BeautifulSoup Find

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# intial
+# Day 1
+from bs4 import BeautifulSoup 
+import requests
+import re
+
+URL = 'https://en.wikipedia.org/wiki/Harry_Potter'
+
+page = requests.get(URL)
+
+#print(page.content)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+regex = re.compile('^tocsection-')
+content_lis = soup.find_all('li', attrs={'class': regex})
+print(content_lis)
+
+
+
